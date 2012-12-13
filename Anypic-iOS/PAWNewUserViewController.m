4fefa3989038f0557de9f8f83bc98680dac4c62a
Harta Wijaya <harta.wijaya.11.10@gmail.com>
2012-12-13 16:07:15 +0800

    Sign up

--- Anypic-iOS/PAWNewUserViewController.m
+++ Anypic-iOS/PAWNewUserViewController.m
@@ -10,6 +10,9 @@
 
 #import <Parse/Parse.h>
 #import "PAWActivityView.h"
+#import "UIImage+ResizeAdditions.h"
+#import <QuartzCore/QuartzCore.h>
+
 //#import "PAWWallViewController.h"
 //#import "TabBarController.h"
 
@@ -26,6 +29,8 @@
 @synthesize doneButton;
 @synthesize usernameField;
 @synthesize passwordField, passwordAgainField;
+@synthesize profileImageCaptureButton;
+@synthesize profileImage;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -50,13 +55,18 @@
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textInputChanged:) name:UITextFieldTextDidChangeNotification object:usernameField];
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textInputChanged:) name:UITextFieldTextDidChangeNotification object:passwordField];
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textInputChanged:) name:UITextFieldTextDidChangeNotification object:passwordAgainField];
-
+    
+    profileImage = [UIImage imageNamed:@"default_profile.jpg"];
+    profileImageCaptureButton.image = profileImage;
+    [profileImageCaptureButton setUserInteractionEnabled:YES];
+    UITapGestureRecognizer *tapper = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(uploadProfile:)];
+    [profileImageCaptureButton addGestureRecognizer:tapper];
 	doneButton.enabled = NO;
 }
 
 - (void)viewDidUnload {
     [super viewDidUnload];
-
+    
 	[[NSNotificationCenter defaultCenter] removeObserver:self name:UITextFieldTextDidChangeNotification object:usernameField];
 	[[NSNotificationCenter defaultCenter] removeObserver:self name:UITextFieldTextDidChangeNotification object:passwordField];
 	[[NSNotificationCenter defaultCenter] removeObserver:self name:UITextFieldTextDidChangeNotification object:passwordAgainField];
@@ -79,6 +89,18 @@
 	[[NSNotificationCenter defaultCenter] removeObserver:self name:UITextFieldTextDidChangeNotification object:passwordAgainField];
 }
 
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    NSLog(@"TTT");
+    [self dismissModalViewControllerAnimated:NO];
+    profileImage = [info objectForKey:UIImagePickerControllerEditedImage];
+    
+    profileImageCaptureButton.image = profileImage;
+}
+
 #pragma mark - UITextFieldDelegate methods
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
@@ -92,7 +114,7 @@
 		[passwordAgainField resignFirstResponder];
 		[self processFieldEntries];
 	}
-
+    
 	return YES;
 }
 
@@ -128,6 +150,101 @@
 	[self processFieldEntries];
 }
 
+- (BOOL)shouldStartCameraController {
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] == NO) {
+        return NO;
+    }
+    
+    UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]
+        && [[UIImagePickerController availableMediaTypesForSourceType:
+             UIImagePickerControllerSourceTypeCamera] containsObject:(NSString *)kUTTypeImage]) {
+        
+        cameraUI.mediaTypes = [NSArray arrayWithObject:(NSString *) kUTTypeImage];
+        cameraUI.sourceType = UIImagePickerControllerSourceTypeCamera;
+        
+        if ([UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceRear]) {
+            cameraUI.cameraDevice = UIImagePickerControllerCameraDeviceRear;
+        } else if ([UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceFront]) {
+            cameraUI.cameraDevice = UIImagePickerControllerCameraDeviceFront;
+        }
+        
+    } else {
+        return NO;
+    }
+    
+    cameraUI.allowsEditing = YES;
+    cameraUI.showsCameraControls = YES;
+    cameraUI.delegate = self;
+    [self presentModalViewController:cameraUI animated:YES];
+    
+    return YES;
+}
+
+
+- (BOOL)shouldStartPhotoLibraryPickerController {
+    if (([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary] == NO
+         && [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeSavedPhotosAlbum] == NO)) {
+        return NO;
+    }
+    
+    UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]
+        && [[UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypePhotoLibrary] containsObject:(NSString *)kUTTypeImage]) {
+        
+        cameraUI.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        cameraUI.mediaTypes = [NSArray arrayWithObject:(NSString *) kUTTypeImage];
+        
+    } else if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeSavedPhotosAlbum]
+               && [[UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeSavedPhotosAlbum] containsObject:(NSString *)kUTTypeImage]) {
+        
+        cameraUI.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+        cameraUI.mediaTypes = [NSArray arrayWithObject:(NSString *) kUTTypeImage];
+        
+    } else {
+        return NO;
+    }
+    
+    cameraUI.allowsEditing = YES;
+    cameraUI.delegate = self;
+    [self presentModalViewController:cameraUI animated:YES];
+    
+    return YES;
+}
+
+- (BOOL)shouldPresentPhotoCaptureController {
+    BOOL presentedPhotoCaptureController = [self shouldStartCameraController];
+    if (!presentedPhotoCaptureController) {
+        presentedPhotoCaptureController = [self shouldStartPhotoLibraryPickerController];
+    }
+    
+    return presentedPhotoCaptureController;
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 0) {
+        [self shouldStartCameraController];
+    } else if (buttonIndex == 1) {
+        [self shouldStartPhotoLibraryPickerController];
+    }
+}
+
+- (IBAction)uploadProfile:(id)sender {
+    BOOL cameraDeviceAvailable = [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera];
+    BOOL photoLibraryAvailable = [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary];
+    
+    if (cameraDeviceAvailable && photoLibraryAvailable) {
+        UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"Take Photo", @"Choose Photo", nil];
+        [actionSheet showInView:self.view];
+        //[actionSheet showFromTabBar:self.tabBar];
+    } else {
+        // if we don't have at least two options, we automatically show whichever is available (camera or roll)
+        [self shouldPresentPhotoCaptureController];
+    }
+}
+
 #pragma mark - Private methods:
 
 #pragma mark Field validation
@@ -136,7 +253,7 @@
 	// Check that we have a non-zero username and passwords.
 	// Compare password and passwordAgain for equality
 	// Throw up a dialog that tells them what they did wrong if they did it wrong.
-
+    
 	NSString *username = usernameField.text;
 	NSString *password = passwordField.text;
 	NSString *passwordAgain = passwordAgainField.text;
@@ -145,13 +262,13 @@
 	NSString *passwordBlankText = @"enter a password";
 	NSString *joinText = @", and ";
 	NSString *passwordMismatchText = @"enter the same password twice";
-
+    
 	BOOL textError = NO;
-
+    
 	// Messaging nil will return 0, so these checks implicitly check for nil text.
 	if (username.length == 0 || password.length == 0 || passwordAgain.length == 0) {
 		textError = YES;
-
+        
 		// Set up the keyboard for the first field missing input:
 		if (passwordAgain.length == 0) {
 			[passwordAgainField becomeFirstResponder];
@@ -162,11 +279,11 @@
 		if (username.length == 0) {
 			[usernameField becomeFirstResponder];
 		}
-
+        
 		if (username.length == 0) {
 			errorText = [errorText stringByAppendingString:usernameBlankText];
 		}
-
+        
 		if (password.length == 0 || passwordAgain.length == 0) {
 			if (username.length == 0) { // We need some joining text in the error:
 				errorText = [errorText stringByAppendingString:joinText];
@@ -184,14 +301,14 @@
 		[passwordField becomeFirstResponder];
 		goto showDialog;
 	}
-
+    
 showDialog:
 	if (textError) {
 		UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:errorText message:nil delegate:self cancelButtonTitle:nil otherButtonTitles:@"Ok", nil];
 		[alertView show];
 		return;
 	}
-
+    
 	// Everything looks good; try to log in.
 	// Disable the done button for now.
 	doneButton.enabled = NO;
@@ -201,16 +318,25 @@ showDialog:
 	label.font = [UIFont boldSystemFontOfSize:20.f];
 	[activityView.activityIndicator startAnimating];
 	[activityView layoutSubviews];
-
+    
 	[self.view addSubview:activityView];
-
+    
 	// Call into an object somewhere that has code for setting up a user.
 	// The app delegate cares about this, but so do a lot of other objects.
 	// For now, do this inline.
-
+    
 	PFUser *user = [PFUser user];
 	user.username = username;
 	user.password = password;
+    UIImage *resizedImage = [profileImage resizedImageWithContentMode:UIViewContentModeScaleAspectFit bounds:CGSizeMake(560.0f, 560.0f) interpolationQuality:kCGInterpolationHigh];
+    UIImage *thumbnailImage = [profileImage thumbnailImage:86.0f transparentBorder:0.0f cornerRadius:10.0f interpolationQuality:kCGInterpolationDefault];
+    
+    // JPEG to decrease file size and enable faster uploads & downloads
+    NSData *imageData = UIImageJPEGRepresentation(resizedImage, 0.8f);
+    NSData *thumbnailImageData = UIImagePNGRepresentation(thumbnailImage);
+    
+    PFFile *photoFile = [PFFile fileWithData:imageData];
+    [user setObject:photoFile forKey:@"image"];
 	[user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
 		if (error) {
 			UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:[[error userInfo] objectForKey:@"error"] message:nil delegate:self cancelButtonTitle:nil otherButtonTitles:@"Ok", nil];
@@ -222,16 +348,16 @@ showDialog:
 			[usernameField becomeFirstResponder];
 			return;
 		}
-
+        
 		// Success!
 		[activityView.activityIndicator stopAnimating];
 		[activityView removeFromSuperview];
-
+        
 		/*PAWWallViewController *wallViewController = [[PAWWallViewController alloc] initWithNibName:nil bundle:nil];
-		[(UINavigationController *)self.presentingViewController pushViewController:wallViewController animated:NO];*/
+         [(UINavigationController *)self.presentingViewController pushViewController:wallViewController animated:NO];*/
 		
 		/*TabBarController *tabBar = [[TabBarController alloc] init];
-		[(UINavigationController *)self.presentingViewController pushViewController:tabBar animated:NO];*/
+         [(UINavigationController *)self.presentingViewController pushViewController:tabBar animated:NO];*/
 		[self.presentingViewController dismissModalViewControllerAnimated:YES];
 	}];
 }
